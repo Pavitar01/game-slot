b7c0ec0177bad518dabaa0feedb60a234faff5ad
Pavitar Singh <pavitar.1127@zenmonk.tech>
2025-03-25 15:25:51 +0530

    Feature: Init

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from 'react'
+import { Box } from '@mui/material'
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return <Box className="layout"></Box>
+}
+
+export default layout
---
+++ src/modules/home/sections/hero-section/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const HeroSections = () => {
+  return (
+    <Box>
+      
+    </Box>
+  )
+}
+
+export default HeroSections
